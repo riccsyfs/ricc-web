19b3a13ba0b9dd243d17468166f4d4080805332d
Linn Zhao <ricc.syfs@gmail.com>
2026-07-22 00:44:10 -0500

    update

---
+++ apps/desktop/deploys/element-v1.12.24/bundles/12c8185ea2689492c07b/3236.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkelement_web=self.webpackChunkelement_web||[]).push([[3236],{"./src/modules.js"(e,s,l){l.d(s,["INSTALLED_MODULES",0,[]])}}]);
+//# sourceMappingURL=3236.js.map

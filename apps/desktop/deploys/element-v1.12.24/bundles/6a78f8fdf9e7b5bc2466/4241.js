950f3b47ac19572184384288e665ec2469e64616
riccsyfs <ricc.syfs@gmail.com>
2026-07-23 02:33:19 -0500

    add deploy

---
+++ apps/desktop/deploys/element-v1.12.24/bundles/6a78f8fdf9e7b5bc2466/4241.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkelement_web=self.webpackChunkelement_web||[]).push([[4241],{"./src/modules.js"(e,s,l){l.d(s,["INSTALLED_MODULES",0,[]])}}]);
+//# sourceMappingURL=4241.js.map
